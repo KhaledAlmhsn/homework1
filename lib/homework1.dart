ffb9ef86d49ed530465f74f101f2d0602d6f6db4
Khaled Almhsn <khaledalmuhaisen2006@gmail.com>
2026-01-21 11:00:48 +0300

    Homework 1 DONE

---
+++ lib/homework1.dart
@@ -0,0 +1,41 @@
+void main() {
+  // Question 1:
+
+  int swapNumber(int number) {
+    if (number == 0) {
+      return 1;
+    }
+    return 0;
+  }
+
+  // Question 2:
+
+  bool checkLetters(String srt) {
+    String srtLowerCase = srt.toLowerCase();
+    String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < alphabet.length; i++) {
+      if (!srtLowerCase.contains(alphabet[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Question 3:
+
+  void countLetters(String str) {
+    Map<String, int> map = {};
+
+    for (int j = 0; j < str.length; j++) {
+      var letter = str[j];
+      if (map.containsKey(letter)) {
+        map[letter] = map[letter]! + 1;
+        continue;
+      }
+      map[letter] = 1;
+    }
+
+    print(map);
+  }
+}
